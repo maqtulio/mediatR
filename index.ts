4e7f0e60fa9d0ceaff6d7c6ac3ab78af477c73ee
Tulio Duarte <maqtulio@gmail.com>
2021-02-20 02:01:00 -0300

    initial commit

--- index.ts
+++ index.ts
@@ -1,1 +1,1 @@
-export { MediatR, IHandler, registerHandlers, CommandHandler, registeredCommands } from './src/index.js';
+export { Command,MediatR, IHandler, registerHandlers, CommandHandler, registeredCommands } from './src/index.js';
